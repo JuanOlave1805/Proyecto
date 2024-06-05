fc0a0882058730f5864a454696b05f74d4bff5e6
Juan Olave <olavejuan1805@gmail.com>
2024-06-04 20:52:45 -0500

    comienzo de cards

--- JS/nav bar.js
+++ JS/nav bar.js
@@ -4,3 +4,12 @@ const mainMenuElement = document.getElementById("main-menu");
 toggleMenuElement.addEventListener("click", () => {
   mainMenuElement.classList.toggle("main-menu--show");
 });
+
+window.addEventListener("scroll", function(){
+  var header = document.querySelector("header");
+  header.classList.toggle("bg-white",window.scrollY>0);
+})
+window.addEventListener("scroll", function(){
+  var header = document.querySelector("header");
+  header.classList.toggle("bg-white",window.scrollY>0);
+})

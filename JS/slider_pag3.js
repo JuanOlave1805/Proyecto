5a40a24003edf45b04fdb8766c2733a1823fb971
Yariangel <olavejuan1805@gmail.com>
2024-06-13 13:52:32 -0500

    probando slider

--- JS/slider_pag3.js
+++ JS/slider_pag3.js
@@ -1,5 +1,16 @@
+let slidesToShow = 4; // Por defecto, muestra 4 slides por vista
+
+if (window.matchMedia("(max-width: 720px)").matches) {
+    slidesToShow = 1;
+} else if (window.matchMedia("(max-width: 1120px)").matches) {
+    slidesToShow = 2;
+} else if (window.matchMedia("(max-width: 1720px)").matches) {
+    slidesToShow = 3;
+} else {
+    slidesToShow = 4;
+} 
 let swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
+  slidesPerView: slidesToShow,
   spaceBetween: 30,
   autoplay: {
     delay: 2500,
@@ -10,3 +21,5 @@ let swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+// Slider

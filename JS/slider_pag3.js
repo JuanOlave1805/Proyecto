827ff5fdaf9a08db6f9e7a46062c3188d0351c00
Juan Olave <olavejuan1805@gmail.com>
2024-06-21 17:43:16 -0500

    solucionando el slider

--- JS/slider_pag3.js
+++ JS/slider_pag3.js
@@ -1,24 +1,23 @@
-let slidesToShow = 4; // Por defecto, muestra 4 slides por vista
-
-if (window.matchMedia("(max-width: 720px)").matches) {
-    slidesToShow = 1;
-} else if (window.matchMedia("(max-width: 1120px)").matches) {
-    slidesToShow = 2;
-} else if (window.matchMedia("(max-width: 1980px)").matches) {
-    slidesToShow = 3;
-} else {
-    slidesToShow = 4;
-} 
-let swiper = new Swiper(".mySwiper", {
-  slidesPerView: slidesToShow,
-  spaceBetween: 30,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
   },
 });
 
